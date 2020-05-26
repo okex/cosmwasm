7932b87a8a31126dde1ced089b85c9385ce6f24e
Simon Warta <simon@warta.it>
2020-05-26 16:34:19 +0200

    Update test contracts for VM

--- packages/vm/src/instance.rs
+++ packages/vm/src/instance.rs
@@ -618,7 +618,7 @@ mod singlepass_test {
 
         let handle_used = gas_before_handle - instance.get_gas_left();
         println!("handle used: {}", handle_used);
-        assert_eq!(handle_used, 95374);
+        assert_eq!(handle_used, 96631);
     }
 
     #[test]
@@ -653,6 +653,6 @@ mod singlepass_test {
 
         let query_used = gas_before_query - instance.get_gas_left();
         println!("query used: {}", query_used);
-        assert_eq!(query_used, 32750);
+        assert_eq!(query_used, 32626);
     }
 }
